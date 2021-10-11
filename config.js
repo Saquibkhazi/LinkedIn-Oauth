9400016cf5dcbe6966d341d66808f8f1babb9cda
saquib <Saquibkhazi>
2021-10-11 14:39:36 +0530

    init cmd

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '86vx5m5x5oyw6l', // your App ID
+      'clientSecret': 'ejNqpPVqneKJsmaF', // your App Secret
+      'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
+    }
+  }
